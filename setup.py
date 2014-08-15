29d1b36dbef2204437fb267449dd142292047b45
Erik Telepovský <erik.telepovsky@gmail.com>
2014-08-15 14:55:08 +0200

    setup, requirements, readme, manifest, .gitignore

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='django-notifications',
+    version='0.1.0',
+    description='Django notifications',
+    long_description=open('README.rst').read(),
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-notifications',
+    packages=[
+        'notifications',
+    ],
+    include_package_data=True,
+    install_requires=('django', ),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha'
+    ],
+    license='BSD License',
+    keywords = "django notifications",
+)
